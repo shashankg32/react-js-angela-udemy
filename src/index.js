ebe7d21d16b82698292580a7d32380732c749798
shashankg <shashank6511@gamil.com>
2023-02-14 10:34:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>hello world</h1>
+    <p>this is a pragraph</p>
+  </div>, document.getElementById("root")
+  );
